b76f379baa65c5db6a6690f46e2843f773bd0753
M1S7K <shumilinms@gmail.com>
2022-12-10 16:21:46 +0000

    chore: table should not have a type column

--- delete_data.sh
+++ delete_data.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
-echo "$($PSQL "DELETE FROM properties WHERE atomic_number = 1000;")"
-echo "$($PSQL "DELETE FROM elements WHERE atomic_number = 1000;")"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -c"
+#echo "$($PSQL "SELECT * FROM properties")"
+#echo "$($PSQL "ALTER TABLE properties DROP COLUMN type")"
+#echo "$($PSQL "SELECT * FROM properties")"
+#echo "$($PSQL "DELETE FROM properties WHERE atomic_number = 1000;")"
+#echo "$($PSQL "DELETE FROM elements WHERE atomic_number = 1000;")"
