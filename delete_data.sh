22fe008ca6f19099502c589c93e04ffb72a57df7
M1S7K <shumilinms@gmail.com>
2022-12-10 16:57:56 +0000

    refactor: deleted temporary files

--- delete_data.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table -c"
-#echo "$($PSQL "SELECT * FROM properties")"
-#echo "$($PSQL "ALTER TABLE properties DROP COLUMN type")"
-#echo "$($PSQL "SELECT * FROM properties")"
-#echo "$($PSQL "DELETE FROM properties WHERE atomic_number = 1000;")"
-#echo "$($PSQL "DELETE FROM elements WHERE atomic_number = 1000;")"
